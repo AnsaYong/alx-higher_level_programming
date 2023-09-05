d93d8283734e4c902d412ba1494f90933f610ad9
AnsaYong <ansahmbomyong@gmail.com>
2023-09-05 22:06:03 +0200

    updating 1-last_digit.py to properly handle negative cases

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = number % 10
-if last_digit < 6 and last_dgit != 0:
+if number < 0:
+    last_digit = (-1) * abs(number) % 10
+elif number > 0:
+    last_digit = number % 10
+else:
+    last_digit = 0
+if last_digit < 6 and last_digit != 0:
     print("Last digit of {} is {} and is "
           "less than 6 and not 0".format(number, last_digit))
 elif last_digit == 0:
