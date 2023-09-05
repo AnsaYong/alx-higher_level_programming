9c59fc814e9fda7e497ebcd0fecfe134c2d6ff9d
AnsaYong <ansahmbomyong@gmail.com>
2023-09-05 21:56:31 +0200

    adding several functions

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        if ord(str[i]) > 96 and ord(str[i]) < 123:
+            print("{}".format(chr(ord(str[i]) - 32)),
+                  end="" if i < (len(str) - 1) else "\n")
+        else:
+            print("{}".format(chr(ord(str[i]))),
+                  end="" if i < (len(str) - 1) else "\n")
