57beceabc48bb88c91d0ca98cdb723e8936e4a78
AnsaYong <ansahmbomyong@gmail.com>
2023-09-08 00:35:06 +0200

    adding 0-add.py which imports and uses a module. It is a module itself so a condition is added to ensure it is only executed if it is ran as the main script

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import add_0 as add
+from add_0 import add
 
 if __name__ == "__main__":
     a = 1
     b = 2
 
-    sum = add.add(a, b)
+    sum = add(a, b)
 
     print("{} + {} = {}".format(a, b, sum))
