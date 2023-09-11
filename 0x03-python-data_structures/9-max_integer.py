ba92158cb0ea5b77d1b70a86d24e8757c704b87f
AnsaYong <ansahmbomyong@gmail.com>
2023-09-11 22:13:18 +0200

    adding more simple python functions

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if len(my_list) == 0:
-        return None
+        return (None)
 
     my_list.sort()
 
-    return(my_list[(len(my_list) - 1)])
+    return (my_list[(len(my_list) - 1)])
