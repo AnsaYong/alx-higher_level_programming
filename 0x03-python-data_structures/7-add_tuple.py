8c947a1d7a9a04f1d5525a6f927c20fe4b71dbec
AnsaYong <ansahmbomyong@gmail.com>
2023-09-11 22:45:40 +0200

    updating functions to handle more exceptions

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,22 +1,10 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) == 0:
-        x = 0
-        y = 0
-    elif len(tuple_a) == 1:
-        x = tuple_a[0]
-        y = 0
-    else:
-        x, y = tuple_a
+    x = tuple_a[0] if len(tuple_a) > 0 else 0
+    y = tuple_a[1] if len(tuple_a) > 1 else 0
 
-    if len(tuple_b) == 0:
-        a = 0
-        b = 0
-    elif len(tuple_b) == 1:
-        a = tuple_b[0]
-        b = 0
-    else:
-        a, b = tuple_b
+    a = tuple_b[0] if len(tuple_b) > 0 else 0
+    b = tuple_b[1] if len(tuple_b) > 1 else 0
 
     t = (a + x, b + y)
 
