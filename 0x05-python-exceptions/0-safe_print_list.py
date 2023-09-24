8933dabc51a69ebffcef7edf0be309ba8a287060
AnsaYong <ansahmbomyong@gmail.com>
2023-09-24 11:49:48 +0200

    adding a file that handles an IndeError exception

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    n = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="" if i < x - 1 else "\n")
+            n += 1
+        except IndexError:
+            print("\n", end="")
+            break
+
+    return (n)
