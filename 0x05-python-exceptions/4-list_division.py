9aee83352f2d623e3f6a85162ee15341c2a667ec
AnsaYong <ansahmbomyong@gmail.com>
2023-09-25 16:21:28 +0200

    editing the message displayed when ZeroDivisionError exception is raised - changing 'dvision by 0' to 'division by 0'

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -5,7 +5,7 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             new_elt = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
-            print("dvision by 0")
+            print("division by 0")
             new_elt = 0
         except TypeError:
             print("wrong type")
