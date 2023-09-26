b4218bf16e9db13b38e3d877ea4e32f2709ef184
AnsaYong <ansahmbomyong@gmail.com>
2023-09-27 01:09:24 +0200

    instantiating Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module docstring.
+
+This module creates a class that defines a square.
+The class is empty for now.
+
+"""
+
+
+class Square:
+    """Class Square
+
+    Defines a square.
+    """
+    def __init__(self, size):
+        """Initialization method.
+
+        Instantiates a private instance attribute
+        with the argumment size, provided by the user.
+
+        """
+        self.__size = size
