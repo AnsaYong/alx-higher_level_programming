211ef67a2ad24e1b2177c7c09eb1a99eb77732f8
AnsaYong <ansahmbomyong@gmail.com>
2023-10-02 16:09:47 +0200

    adding an empty class, rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module provides an empty class
+The class is called Rectangle.
+"""
+
+
+class Rectangle:
+    """Class Rectangle
+    Defines a Rectangle.
+    """
+    pass
