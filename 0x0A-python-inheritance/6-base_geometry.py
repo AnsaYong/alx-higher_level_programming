6cb69969f335e15e57654f57a3afb051075f439a
AnsaYong <ansahmbomyong@gmail.com>
2023-10-09 15:15:24 +0200

    added a public instance method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+A module that provides a class
+BaseGeometry. It computes calculations
+for simple geometries.
+"""
+
+
+class BaseGeometry:
+    """
+    A class that computes simple
+    geometry.
+    """
+    def area(self):
+        """
+        A public instance method which raises
+        an exception.
+
+        Raises:
+            Exception: as long as area() is not implemented
+        """
+        raise Exception("area() is not implemented")
