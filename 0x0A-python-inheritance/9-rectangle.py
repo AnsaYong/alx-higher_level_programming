7ecc829931d9b28cdd0f3cec9806833ba9800489
AnsaYong <ansahmbomyong@gmail.com>
2023-10-09 17:17:59 +0200

    added an area method and a str method for the class Rectangle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -36,16 +36,10 @@ class BaseGeometry:
             raise ValueError("{} must be greater than 0".format(name))
 
 
-"""
-This module creates a class that inherits
-from another class.
-"""
-
-
 class Rectangle(BaseGeometry):
     """
     Class Rectangle.
-    Inherits from class BaseGeometry
+    Inherits __from class BaseGeometry
     and defines a square.
     """
     def __init__(self, width, height):
@@ -59,3 +53,18 @@ class Rectangle(BaseGeometry):
         super().integer_validator("height", height)
         self.__width = width
         self.__height = height
+
+    def area(self):
+        """Calculates the area of a rectangle
+
+        Returns:
+            the area
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        Returns a string representation of the
+        rectangle
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
