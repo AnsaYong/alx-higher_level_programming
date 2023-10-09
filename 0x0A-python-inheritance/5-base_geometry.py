76350f4e57b2981bad462f6663edb0e8618104f2
AnsaYong <ansahmbomyong@gmail.com>
2023-10-09 15:07:26 +0200

    adding an empty class

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+A module that provides a class
+BaseGeometry. It computes calculate
+for simple features.
+"""
+
+
+class BaseGeometry:
+    """
+    A class that computes simple
+    geometry..
+    """
+    pass
