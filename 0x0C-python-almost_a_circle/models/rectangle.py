aaf5ed5ea5e88cef4b96badfa4984d423c3f9c4c
AnsaYong <ansahmbomyong@gmail.com>
2023-10-18 11:34:09 +0200

    undid the previous changes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,27 +4,7 @@ This module provides the ``Rectangle`` class which
 inherits _from the ``Base`` class.
 """
 
-
-class Base:
-    """
-    The Base class has a private attribute
-    which will be used to set ``id`` when
-    it is not given.
-    """
-
-    __nb_objects = 0
-
-    def __init__(self, id=None):
-        """
-        Initializes the Base class based on
-        the value of the ``id`` argument.
-        """
-
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+from models.base import Base
 
 
 class Rectangle(Base):
