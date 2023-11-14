52e8bf91cd8e1586a574f17d0dd00d1bd375c222
AnsaYong <ansahmbomyong@gmail.com>
2023-11-14 21:51:54 +0200

    added an SQL script that lists all the tables of a database

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,11 +1,2 @@
 -- SQL script with a stored procedure to list all tables in a given database
-
-DELIMITER //
-CREATE PROCEDURE ListTables(IN target_database VARCHAR(255))
-BEGIN
-    -- List all tables in the specified database
-    SELECT table_name
-    FROM information_schema.tables
-    WHERE table_schema = target_database;
-END //
-DELIMITER ;
+SHOW TABLES
