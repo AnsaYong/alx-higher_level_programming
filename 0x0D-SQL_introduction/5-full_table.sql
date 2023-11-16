4a0326b5d0da3a2e9cb123c0aa30d6fc8dbc20c9
AnsaYong <ansahmbomyong@gmail.com>
2023-11-16 20:31:02 +0200

    added script that retrieves a table description from a table without using DESCRIBE or EXPLAIN

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,2 @@
--- Get table description by querying the system tables that store
--- information about the database schema
-SELECT COLUMN_NAME, DATA_TYPES, IS_NULLABLE, CHARACTER_MAXIMUM_LENGTH
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = first_table;
+-- Get table description without using DESCRIBE or EXPLAIN
+SHOW CREATE TABLE first_table;
