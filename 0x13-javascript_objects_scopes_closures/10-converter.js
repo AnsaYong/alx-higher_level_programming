10ae37d2544dfb703be167932c1c9b1d16ea0c04
AnsaYong <ansahmbomyong@gmail.com>
2023-12-13 15:53:07 +0200

    updated 10-converter.js to remove extra variable created

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
-  const digits = '0123456789abcdef';
-
   function convertToBase (number) {
     if (number < base) {
-      return digits.charAt(number);
+      return '0123456789abcdef'.charAt(number);
     }
-    return convertToBase(Math.floor(number / base)) + digits.charAt(number % base);
+    return convertToBase(Math.floor(number / base)) + '0123456789abcdef'.charAt(number % base);
   }
 
   return convertToBase;
