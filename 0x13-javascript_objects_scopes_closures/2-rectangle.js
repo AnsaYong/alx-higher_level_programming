0cc8ebbc120366abef3c6ca812801922cdff3d12
AnsaYong <ansahmbomyong@gmail.com>
2023-12-13 02:18:40 +0200

    added 2-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+class Rectangle {
+  // Initialize 2 instance attributes
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
