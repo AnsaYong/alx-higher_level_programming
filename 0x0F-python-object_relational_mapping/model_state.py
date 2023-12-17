3364134ece6f56b61cd5b9d9f5e2ed47a5477fe3
AnsaYong <ansahmbomyong@gmail.com>
2023-12-17 20:51:07 +0200

    added model_state.py

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module provides a class `State` which inherits from the
+`Base` SQLalchemy class and connects to a MySQL table `states`.
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+# Create instance of declarative_base()
+Base = declarative_base()
+
+class State(Base):
+    """Links to a mysql table
+    """
+    __tablename__ = 'states'      # Link to the mysql table `states`
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
