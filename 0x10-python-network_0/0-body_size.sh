10ba3437d5c29e4291d3841a3e52583a44bb1afb
AnsaYong <ansahmbomyong@gmail.com>
2024-01-06 01:00:45 +0200

    updated 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Uses curl to send a request to a URL and then displays the size of the body of the response
-curl -sI "$1" | wc -c
+curl -sI "$1" | grep Content-Length | cut -d " " -f2
