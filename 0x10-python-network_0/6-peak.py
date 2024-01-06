9806f5a6f1b102e7225cb5be1bc28a1b8b3dc6bd
AnsaYong <ansahmbomyong@gmail.com>
2024-01-06 10:55:38 +0200

    added 6-peak.py and 6-peak.txt

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+This module provides a function that identifies the peak
+from a list of unsorted numbers"""
+
+def find_peak(list_of_integers):
+    """
+    Finds a peak in a list of unsorted integers using binary search.
+    
+    Args:
+        - list_of_integers: A list of unsorted integers.
+        
+    Returns:
+        - The peak element
+    """
+    if not list_of_integers:
+        return None     # Handle empty list
+    
+    low, high = 0, len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+
+        if list_of_integers[mid] >= list_of_integers[mid + 1]:
+            # Peak may be on the left half
+            high = mid
+        else:
+            # Peak must be on the right half
+            low = mid + 1
+
+    # 'low' is the inde of the peak
+    return list_of_integers[low]
