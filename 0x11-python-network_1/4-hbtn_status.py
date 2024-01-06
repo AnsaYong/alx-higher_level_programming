3fe9b091bdf81855a9183fcbd3501e57d19cf9bc
AnsaYong <ansahmbomyong@gmail.com>
2024-01-06 21:43:59 +0200

    added 4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module uses `requests` to fetch
+`https://alx-intranet.hbtn.io/status`.
+"""
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+
+    response = requests.get(url)
+
+    content = response.content                  # Retrieve the content
+    utf8_content = content.decode('utf-8')      # Convert content to utf-8
+
+    print("Body response:")
+    print("\t- type:", type(content))
+    print("\t- content:", content)
+    print("\t- utf8 content:", utf8_content)
