af5f1cefd0e31e61211bf07546b9592983f97354
AnsaYong <ansahmbomyong@gmail.com>
2024-01-31 20:42:00 +0200

    updated 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,45 +1,24 @@
 #!/usr/bin/node
+// Script that computes the number of tasks completed by user id
 
 const request = require('request');
+const url = process.argv[2];
 
-// Check if the API URL is provided as a command-line argument
-if (process.argv.length < 3) {
-  console.error('Please provide the API URL.');
-  process.exit(1); // Exit with a non-zero status to indicate an error
-}
-
-const apiUrl = process.argv[2];
-
-// Make a GET request to the API to get the list of tasks
-request.get(apiUrl, (error, response, body) => {
+request(url, (error, _, body) => {
   if (error) {
-    // If an error occurred during the request, print the error message
-    console.error(error.message);
-  } else if (response.statusCode !== 200) {
-    // If the request was not successful, print the status code
-    console.error(response.statusMessage);
+    console.error(error);
   } else {
-    try {
-      // Parse the JSON response
-      const tasksData = JSON.parse(body);
+    const result = {};
 
-      // Compute the number of completed tasks for each user
-      const completedTasksByUser = {};
-      tasksData.forEach(task => {
-        if (task.completed) {
-          if (completedTasksByUser[task.userId]) {
-            completedTasksByUser[task.userId]++;
-          } else {
-            completedTasksByUser[task.userId] = 1;
-          }
+    JSON.parse(body).forEach((data) => {
+      if (data.completed) {
+        if (!(data.userId in result)) {
+          result[data.userId] = 0;
         }
-      });
+        result[data.userId] += 1;
+      }
+    });
 
-      // Print users with completed tasks
-      console.log(JSON.stringify(completedTasksByUser, null, 2));
-    } catch (parseError) {
-      // If an error occurred during parsing, print the error message
-      console.error(`Error parsing the response: ${parseError.message}`);
-    }
+    console.log(result);
   }
 });
